663670b0332b4f92ffa7a02dd93785fc31fb138f
loveltyoic <loveltyoic@gmail.com>
2013-08-28 17:54:23 +0800

    masonry修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,19 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 50.times do |n|
   Product.create(
-    title:    "小米2/2S原装电池",
+    title:    "跑车",
     tag:      "出售",
-    description:  '自己已经有两块原电了，转让订单，但是订单不满100需要10块钱邮费.明天10点失效。需要的站内联系。',
-    picture: ['http://bbs.ustc.edu.cn/cgi/sf?s=9ad67e37&bn=SecondHand&fn=M521C0395&an=2013-08-25+08.21.20_%B8%B1%B1%BE.jpg','http://bbs.ustc.edu.cn/cgi/sf?s=9ad67e37&bn=SecondHand&fn=M521C2020&an=IMG_20130827_113936.jpg'],
+    description:  '玛莎拉蒂（Maserati）是一家意大利豪华汽车制造商，1914年12月1日成立于博洛尼亚（Bologna），公司总部现设于摩德纳（Modena），品牌的标志为一支三叉戟。1993年菲亚特（Fiat S.p.A.）收购玛莎拉蒂，但品牌得以保留。而今的玛莎拉蒂全新轿跑系列是意大利顶尖轿跑车制作技术的体现，也是意大利设计美学以及优质工匠设计思维的完美结合。',
+    picture: ['car1.jpg','car4.jpg','car5.jpg'],
     url: 'http://www.fluidcars.com')
 end
 
-Product.all.each do |product|
-  5.times do 
-    product.comments.create!(
-      content: '价格不错，顶',
-      flag: '赞')
-  end
-end
+# Product.all.each do |product|
+#   5.times do 
+#     product.comments.create!(
+#       content: '价格不错，顶',
+#       flag: '赞')
+#   end
+# end
 
   
